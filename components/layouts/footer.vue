e4bb8a8edd64dfc7e316f4bd4335d2121ab5cad1
uemuragame5683 <nobuyoshi5683@gmail.com>
2021-04-06 23:44:53 +0900

    フッター調整

--- components/layouts/footer.vue
+++ components/layouts/footer.vue
@@ -2,13 +2,14 @@
   <footer>
     <div class="footer-innter">
       <div class="footer-left">
-        <p>(c) 2020 Uemu-nublog</p>
+        <p>(c) 2021 Uemu-nublog</p>
       </div>
       <div class="footer-right">
         <ul>
           <li><a href="/profile/">Profile</a></li>
           <li><a href="/contact/">Contact</a></li>
           <li><a href="https://twitter.com/uemuragame5683" target="_blank">SNS</a></li>
+          <li><a href="https://uemu-engineer.com/" target="_blank">Nu Stack</a></li>
         </ul>
       </div>
     </div>
